dcf22ffc73cd32eadfa125c8381c473b358d5407
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-05 21:42:07 +0300

    styling

--- src/Pages/Wall/Login.js
+++ src/Pages/Wall/Login.js
@@ -9,7 +9,7 @@ function Login() {
   return (
     <div className='flex flex-col justify-center items-center md:justify-center gap-y-3 '>
       <p className='text-gray-300 lg:text-xl'>Leave a souvenir...</p>
-        <button className= 'flex border-4 flex-col items-center mt-12 px-2 py-2 rounded-lg justify-center text-orange-600 hover:bg-red-100 w-32 ' onClick={login}> 
+        <button className= 'flex border-4 flex-col items-center mt-12 px-2 py-2 rounded-lg justify-center text-orange-600 hover:bg-red-100 w-64 ' onClick={login}> 
             Login with Google
             <FcGoogle/>
         </button>
