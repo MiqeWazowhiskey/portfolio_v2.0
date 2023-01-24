1f2549e10bed89129a97a5aa285e869ad05083ff
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-01-24 15:45:03 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import {initializeApp} from 'firebase/app'
 import { getAuth, GoogleAuthProvider  } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: "board-842ea.firebaseapp.com",
-    projectId: "board-842ea",
-    storageBucket: "board-842ea.appspot.com",
-    messagingSenderId: "151946550151",
-    appId: "1:151946550151:web:fc566b5b6e952873ce1122"
-}
- 
 
-const app = initializeApp(firebaseConfig)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "board-842ea.firebaseapp.com",
+  projectId: "board-842ea",
+  storageBucket: "board-842ea.appspot.com",
+  messagingSenderId: "151946550151",
+  appId: "1:151946550151:web:fc566b5b6e952873ce1122"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app)
 
